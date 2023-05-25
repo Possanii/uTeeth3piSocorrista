96cde820aa8d378f4730c5369490a83748d0d258
Possanii <gpossanii@outlook.com>
2023-05-25 17:06:52 -0300

    StreamBuilder firebase

--- lib/pages/EmergencyAccept.dart
+++ lib/pages/EmergencyAccept.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class EmergencyAccept extends StatefulWidget {
@@ -11,39 +12,56 @@ class EmergencyAccept extends StatefulWidget {
 }
 
 class _EmergencyAcceptState extends State<EmergencyAccept> {
-
   var user = FirebaseAuth.instance.currentUser;
 
-  Future<void> getAllEmergencyAccept()async {
-    var doc = await FirebaseFirestore.instance.collection("Chamados").where("uidSocorrista", isEqualTo: user?.uid).where('emergencyAcceptBy', isNotEqualTo: null);
-
-    print(doc);
+  attList() {
+    Timer(Duration(seconds: 5), () => setState(() {}));
   }
 
   @override
   Widget build(BuildContext context) {
+    attList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dentistas aceitos"),
+      ),
+      body: SafeArea(
+          child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 10,
+        ),
+        child: Column(children: [
+          StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection("Chamados")
+                  .where("uidSocorrista", isEqualTo: user?.uid)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.active) {
+                  if (snapshot.hasData) {
+                    QuerySnapshot dataSnapshot = snapshot.data as QuerySnapshot;
 
-    getAllEmergencyAccept();
+                    if (dataSnapshot.docs.length < 1) {
+                      return const Text("Esperando Dentistas aceitarem...");
+                    }
+                    Map<String, dynamic> userMap =
+                        dataSnapshot.docs[0].data() as Map<String, dynamic>;
 
-    return ListView(
-      padding: const EdgeInsets.all(8),
-      children: <Widget>[
-        Container(
-          height: 50,
-          color: Colors.amber[600],
-          child: const Center(child: Text('Entry A')),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber[500],
-          child: const Center(child: Text('Entry B')),
-        ),
-        Container(
-          height: 50,
-          color: Colors.amber[100],
-          child: const Center(child: Text('Entry C')),
-        ),
-      ],
+                    return ListTile(
+                        title: Text(
+                            userMap.values.elementAt(5).toString() as String));
+                  } else if (snapshot.hasError) {
+                    return const Text("Algum erro aconteceu!");
+                  } else {
+                    return const Text("Nenhuma informação encontrada.");
+                  }
+                } else {
+                  return const CircularProgressIndicator();
+                }
+              }),
+        ]),
+      )),
     );
   }
 }
